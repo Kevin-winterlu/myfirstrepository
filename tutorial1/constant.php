8631163ef27eac6c9363c3c01f517102c0126c9d
Kevin winterlu <65181319+situmorang123@users.noreply.github.com>
2020-05-11 23:32:00 +0700

    Add files via upload

---
+++ tutorial1/constant.php
@@ -0,0 +1,44 @@
+<?php 
+
+// define('NAMA', 'kevin winterlu');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 19;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'kevin';
+// }
+
+// echo Coba::NAMA;
+
+
+
+// echo __FILE__;
+
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+
+
+
+
+
+
+
+
+
+?>
